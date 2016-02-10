d852e70537aeb563929701806f2a4912511f9fb5
Laure Leter <laure.leter@gmail.com>
2016-02-10 14:21:28 +0100

    yelp models ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ActiveRecord::Base
   validates :rating, :inclusion => { :in => 0..5, :message => " should be between 0 to 5" }
   validates :rating, :numericality => { :message => " should be a number" }
   validates :rating, :presence => { :message => " cannot be blank" }
+  validates :restaurant_id, :presence => { :message => " cannot be blank" }
 end
